ab8cb6c0d69a9a92955d57184a87c72dc70987a3
Tissera Martín <tissera.martin92@gmail.com>
2020-09-20 17:44:40 -0300

    Comienzo del Proyecto

---
+++ src/Test1.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Test1 {
+	
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] arg) {
+		
+		
+		
+		System.out.println("Ingresar Usuario");
+			
+		String usuario = sc.nextLine();
+		
+		System.out.println("bienvenido " + usuario);
+		
+	}
+
+}
